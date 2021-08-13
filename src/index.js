c0f4c9d677eb64289d916b190c804aafb54953d2
Orko Health Ltd <iqbalahmedtuhin@gmail.com>
2021-08-13 11:08:22 +0600

    I know that how working jsx and react virtual dom. and make watch

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(() => {
+    const element = <h1 className="heading">{new Date().toLocaleTimeString()}</h1>;
+    ReactDOM.render(element, document.getElementById('root'));
+}, 1000);
